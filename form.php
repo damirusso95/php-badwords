13156d0eefbd734e3df7104ad85dec065e87fe53
damirusso95 <damirusso95@gmail.com>
2024-06-10 15:22:21 +0200

    M1: creo due file, uno per il form, l'altro per elaborare

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invio Testo</title>
+</head>
+<body>
+    <form action="elabora.php" method="post">
+        <input type="text" placeholder="scrivi qualcosa" name="testo">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
